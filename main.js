cdedd5c1dfd5ff44a0ab85f6147c3423575b1f28
Robert Suber <robertsuber@Roberts-MacBook-Pro.local>
2016-06-08 01:55:19 -0400

    Its 2AM

--- main.js
+++ main.js
@@ -2,21 +2,21 @@ $(document).ready(function(){
   Todo.init();
 })
 var Todo = {
-  url: 'http://tiny-tiny.herokuapp.com/collections/loganstodosss',
+  url: 'http://tiny-tiny.herokuapp.com/collections/loganstodossss',
   log: [],
   init: function() {
     Todo.styling();
     Todo.events();
   },
   styling: function() {
-    Todo.getlogan();
+Todo.getlogan();
   },
   events: function() {
     $('form').on("submit", function(event) {
       event.preventDefault();
     var todo = $(this).children('input').val();
     console.log(todo);
-    $('.output ul').append(`<li>${todo.todo}<button class="delete">${"delete"}</button></li>`)
+    $('.output ul').append(`<li>${todo}</li>`,`<button class="delete">${"delete"}</button>`)
     $.ajax({
       url:Todo.url,
       method:'POST',
@@ -38,27 +38,29 @@ getlogan: function(){
         console.log("nice",data);
         Todo.log.push(data);
         data.forEach(function(todo) {
-        $('.output ul').append(`<li>${todo.todo}  <button class="delete">${"delete"}</button></li>`);
+        $('.output ul').append(`<li>${todo.todo}</li>`,`<button class="delete">${"delete"}</button>`);
         })
       },
       error: function(err){
         console.log("uhoh",err);
       }
     })
-
 }
 }
-// I have no idea what to do with this code. It will not run no matter where I put it. 
+//This deletes the first item in the list from both the data and the array after you refresh.
+//I have no idea where I am supposed to put this function to get it to work. I put it in my
+//console and it works after I load the page but thats it.
   $('.delete').on('click',function(event){
     $.ajax({
       url:Todo.url + "/" +$(Todo.log[0]).attr('_id'),
       method: 'DELETE',
       success: function(data){
         console.log("deleted",data);
+        console.log($(this));
       },
       error: function(err){
-        (this).remove();
         console.log('uhoh',err);
+        console.log($(this));
       }
     })
   })
