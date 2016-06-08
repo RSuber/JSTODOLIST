6eefde1f74018d5902d1127d29cb1b2a0912e0f3
Robert Suber <robertsuber@Roberts-MacBook-Pro.local>
2016-06-08 01:23:07 -0400

    worked on this for like 5 more hours

--- main.js
+++ main.js
@@ -1,25 +1,22 @@
 $(document).ready(function(){
   Todo.init();
 })
-
 var Todo = {
-  url: 'http://tiny-tiny.herokuapp.com/collections/loganstodos',
+  url: 'http://tiny-tiny.herokuapp.com/collections/loganstodosss',
   log: [],
   init: function() {
     Todo.styling();
     Todo.events();
-    Todo.getlogan();
-    Todo.applydeletebar();
   },
   styling: function() {
-
+    Todo.getlogan();
   },
   events: function() {
     $('form').on("submit", function(event) {
       event.preventDefault();
     var todo = $(this).children('input').val();
     console.log(todo);
-    $('.output ul').append(`<li>${todo}</li>`)
+    $('.output ul').append(`<li>${todo.todo}<button class="delete">${"delete"}</button></li>`)
     $.ajax({
       url:Todo.url,
       method:'POST',
@@ -39,8 +36,9 @@ getlogan: function(){
       method:'get',
         success: function(data){
         console.log("nice",data);
+        Todo.log.push(data);
         data.forEach(function(todo) {
-        $('.output ul').append(`<li>${todo.todo}</li>`)
+        $('.output ul').append(`<li>${todo.todo}  <button class="delete">${"delete"}</button></li>`);
         })
       },
       error: function(err){
@@ -48,22 +46,19 @@ getlogan: function(){
       }
     })
 
-},
-delete: function(Liid){
-  $('button p.clear').on('click',function(event){
+}
+}
+// I have no idea what to do with this code. It will not run no matter where I put it. 
+  $('.delete').on('click',function(event){
     $.ajax({
-      url:'http://tiny-tiny.herokuapp.com/collections/loganstodoss',
+      url:Todo.url + "/" +$(Todo.log[0]).attr('_id'),
       method: 'DELETE',
       success: function(data){
         console.log("deleted",data);
       },
       error: function(err){
+        (this).remove();
         console.log('uhoh',err);
       }
     })
   })
-},
-applydeletebar: function(){
-  $('ul li').append(`<button>${d}</button>`)
-}
-}
