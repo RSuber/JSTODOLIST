77a588b674dc7e2115ed1a16aa93e672d09c3650
Robert Suber <robertsuber@Roberts-MacBook-Pro.local>
2016-06-09 09:43:05 -0400

    new changes

--- main.js
+++ main.js
@@ -1,66 +1,70 @@
-$(document).ready(function(){
-  Todo.init();
+$(document).ready(function() {
+    Todo.init();
 })
 var Todo = {
-  url: 'http://tiny-tiny.herokuapp.com/collections/loganstodossss',
-  log: [],
-  init: function() {
-    Todo.styling();
-    Todo.events();
-  },
-  styling: function() {
-Todo.getlogan();
-  },
-  events: function() {
-    $('form').on("submit", function(event) {
-      event.preventDefault();
-    var todo = $(this).children('input').val();
-    console.log(todo);
-    $('.output ul').append(`<li>${todo}</li>`,`<button class="delete">${"delete"}</button>`)
-    $.ajax({
-      url:Todo.url,
-      method:'POST',
-      data: {todo: todo},
-      success: function(data){
-        console.log("wew",data);
-      },
-      error: function(err){
-        console.log("idiot",err);
-      }
-    })
-    })
-  },
-getlogan: function(){
-  $.ajax({
-      url:Todo.url,
-      method:'get',
-        success: function(data){
-        console.log("nice",data);
-        Todo.log.push(data);
-        data.forEach(function(todo) {
-        $('.output ul').append(`<li>${todo.todo}</li>`,`<button class="delete">${"delete"}</button>`);
+    url: 'http://tiny-tiny.herokuapp.com/collections/loganstodossss',
+    log: [],
+    init: function() {
+        Todo.styling();
+        Todo.events();
+    },
+    styling: function() {
+        Todo.getlogan();
+    },
+    events: function() {
+        $('form').on("submit", function(event) {
+            event.preventDefault();
+            var todo = $(this).children('input').val();
+            console.log(todo);
+            $('.output ul').append(`<li>${todo}</li>`, `<button class="delete">${"delete"}</button>`)
+            $.ajax({
+                url: Todo.url,
+                method: 'POST',
+                data: {
+                    todo: todo
+                },
+                success: function(data) {
+                    console.log("wew", data);
+                },
+                error: function(err) {
+                    console.log("idiot", err);
+                }
+            })
         })
-      },
-      error: function(err){
-        console.log("uhoh",err);
-      }
+        $('.output').on('click','.delete', function(event) {
+      event.preventDefault();
+      todoiD = $(this).parent().data('id');
+      Todo.delete(todoiD);
+      $('.output li')[0].remove();
+      $('button.delete')[0].remove();
     })
-}
-}
-//This deletes the first item in the list from both the data and the array after you refresh.
-//I have no idea where I am supposed to put this function to get it to work. I put it in my
-//console and it works after I load the page but thats it.
-  $('.delete').on('click',function(event){
-    $.ajax({
-      url:Todo.url + "/" +$(Todo.log[0]).attr('_id'),
-      method: 'DELETE',
-      success: function(data){
-        console.log("deleted",data);
-        console.log($(this));
-      },
-      error: function(err){
-        console.log('uhoh',err);
-        console.log($(this));
-      }
-    })
-  })
+
+    },
+    getlogan: function() {
+        $.ajax({
+            url: Todo.url,
+            method: 'get',
+            success: function(data) {
+                console.log("nice", data);
+                Todo.log.push(data);
+                data.forEach(function(todo) {
+                    $('.output ul').append(`<li data-id="${"pickle"}">${todo.todo}</li>`, `<button class="delete">${"delete"}</button>`);
+                })
+            },
+            error: function(err) {
+                console.log("uhoh", err);
+            }
+        })
+    },
+    delete: function(iD) {
+        $.ajax({
+            url: Todo.url + "/" + $(Todo.log[0]).attr('_id'),
+            method: 'DELETE',
+            success: function(data) {
+                console.log("deleted", data);
+            },
+            error: function(err) {
+                console.log('uhoh', err);
+            }
+        })},
+    }
